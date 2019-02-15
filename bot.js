eb8a25b975ef09a91394a4eaa7ca71c9c08a162f
No Nema <44880953+mrdahom4@users.noreply.github.com>
2019-02-15 17:04:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1494,58 +1494,7 @@ https://pastebin.com/PfvYF1ak
 });
 
 
-var prefix = "#"
-client.on('message',async message => {
- 
-let mention = message.mentions.members.first();
- 
-let Room = client.channels.get('545889783529078784');
- 
-if(message.content.startsWith(prefix + "رفض")) {
- 
-if(message.guild.id !== '525434548939653151') return;
- 
- if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
- 
- 
-if(!mention) return message.reply("منشن شخص");
- 
- 
- 
-Room.send(`
-** العضو :** ${mention}
- لقد تم رفضك `);
- 
-}
- 
-});
- 
- 
-var prefix = "#"   
-client.on('message',async message => {
- 
-let mention = message.mentions.members.first();
- 
-let Room = client.channels.get('545889783529078784');
- 
-if(message.content.startsWith(prefix + "قبول")) {
- 
-if(message.guild.id !== '525434548939653151') return;
- 
- if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
- 
- 
-if(!mention) return message.reply("منشن شخص");
- 
- 
- 
-Room.send(`
-** العضو :** ${mention}
- لقد تم قبولك `);
- 
-}
- 
-});
+
 
 
 
